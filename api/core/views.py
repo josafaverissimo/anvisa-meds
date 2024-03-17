cc75d8c5187163bee4fa6e39409aa2abed0b5900
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-03-16 23:30:42 -0300

    add: views

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .repositories import \
+    MedsPriceRepository, \
+    SubstancesRepository, \
+    MedsRepository, \
+    LaboratoriesRepository, \
+    LaboratoriesMedsRepository
+
+
+class MedsPriceView(APIView):
+    def get(self, request):
+        meds_price_repository = MedsPriceRepository()
+        substances_repository = SubstancesRepository()
+        meds_repository = MedsRepository()
+        laboratories_repository = LaboratoriesRepository()
+        laboratories_meds_repository = LaboratoriesMedsRepository()
+        
+        meds_price_rows = meds_price_repository.get_rows_data()
+
+        for row in meds_price_rows:
+            substance_id = substances_repository.save(row['substance'])
+            med_id = meds_repository.save(
+                substance_id=substance_id,
+                registration=row['registration'],
+                fabric_price_no_taxes=row['fabric_price_no_taxes'],
+                max_sale_price_government=row['max_sale_price_government']
+            )
+            laboratory_id = laboratories_repository.save(name=row['laboratory'], cnpj=row['cnpj'])
+            laboratory_med_id = laboratories_meds_repository.save(laboratory_id=laboratory_id, med_id=med_id)
+
+        return Response('Stored')
