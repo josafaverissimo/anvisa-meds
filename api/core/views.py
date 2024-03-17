48ff6a461ff61506feff16a2f5c44d8e28544256
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-03-17 01:20:58 -0300

    feat: PmvgDataInsertView returns response status - It returns 201 when success is true and 500 when success is false

--- api/core/views.py
+++ api/core/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .services import PmvgDataService
 
@@ -8,6 +9,10 @@ class PmvgDataInsertView(APIView):
     def post(self, request):
         pmvg_data_service = PmvgDataService()
 
-        return Response({
-            "success": pmvg_data_service.insert_pmvg_data_in_database()
-        })
+        success_store_pmvg_data_in_data_base = pmvg_data_service.insert_pmvg_data_in_database()
+        response_http_status = status.HTTP_201_CREATED if pmvg_data_service else status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        return Response(
+            data={"success": success_store_pmvg_data_in_data_base},
+            status=response_http_status
+        )
