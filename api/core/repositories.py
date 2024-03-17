bb4af57bed94c5bc27d718d50f85bddd302a0cfb
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-03-17 11:38:24 -0300

    feat: search on tables - Search by laboratory_name - Search by laboratory_cnpj - Search by med_substance - Search on all by full text search

--- api/core/repositories.py
+++ api/core/repositories.py
@@ -4,9 +4,14 @@ import xlrd
 
 from .settings import PMVG_FILE_PATH, PMVG_COLUMNS, MEDS_ITEMS_PER_PAGE
 from .models import Substances, Meds, Laboratories, LaboratoriesMeds
+from .dtos import LaboratoriesMedsSearchDto
 
-from django.db.models import F, Count
+from django.db.models import F, Q, Count
 from django.db.models.functions import Concat
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+
+
+from typing import Optional
 
 
 class MedsPriceRepository:
@@ -35,6 +40,8 @@ class MedsPriceRepository:
 
 class SubstancesRepository:
     def save(self, name: str):
+        name = name.lower()
+
         substance_stored = Substances.objects.filter(name=name).first()
 
         if substance_stored is not None:
@@ -74,6 +81,8 @@ class MedsRepository:
 
 class LaboratoriesRepository:
     def save(self, name: str, cnpj: str) -> int:
+        name = name.lower()
+
         stored_laboratory = Laboratories.objects.filter(cnpj=cnpj).first()
 
         if stored_laboratory is not None:
@@ -104,25 +113,65 @@ class LaboratoriesMedsRepository:
 
         return laboratories_meds.id
 
-    def get_total_pages(self):
-        query = LaboratoriesMeds.objects.select_related('laboratory', 'med__substance') \
-          .annotate(concatenated_name=Concat(F('med__substance__name'), F('laboratory__name'))) \
-          .aggregate(total_pages=Count('concatenated_name', distinct=True))
+    def base_query(self):
+        return LaboratoriesMeds.objects.select_related('laboratory', 'med__substance')
 
-        return math.ceil(query['total_pages'] / self.ITEMS_PER_PAGE)
+    def get_total_pages(self, query):
+        result = query \
+            .annotate(concatenated_name=Concat(F('med__substance__name'), F('laboratory__name'))) \
+            .aggregate(total_pages=Count('concatenated_name', distinct=True))
 
-    def get_by_page(self, page: int = 1):
+        return math.ceil(result['total_pages'] / self.ITEMS_PER_PAGE)
+
+    def get_query(self, search_dto: Optional[LaboratoriesMedsSearchDto] = None):
+        query = self.base_query()
+
+        if search_dto is not None:
+            if search_dto.term != '':
+                search_vector = SearchVector('laboratory__name', 'laboratory__cnpj', 'med__substance__name')
+                search_query = SearchQuery(search_dto.term)
+
+                return query.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query),
+                    similarity_laboratory_name=TrigramSimilarity('laboratory__name', search_dto.term),
+                    similarity_laboratory_cnpj=TrigramSimilarity('laboratory__cnpj', search_dto.term),
+                    similarity_substance_name=TrigramSimilarity('med__substance__name', search_dto.term)
+                ).filter(
+                    Q(search=search_query) |
+                    Q(similarity_laboratory_name__gt=0) |
+                    Q(similarity_laboratory_cnpj__gt=0) |
+                    Q(similarity_substance_name__gt=0)
+                ).order_by(
+                    '-rank',
+                    '-similarity_laboratory_name',
+                    '-similarity_laboratory_cnpj',
+                    '-similarity_substance_name'
+                )
+
+            return query.filter(
+                Q(med__substance__name__icontains=search_dto.med_substance) &
+                Q(laboratory__name__icontains=search_dto.laboratory_name) &
+                Q(laboratory__cnpj__icontains=search_dto.laboratory_cnpj)
+            )
+
+        return query
+
+    def get_by_page(self, page: int = 1, search_dto: Optional[LaboratoriesMedsSearchDto] = None):
+        search_dto = search_dto if search_dto is not None else LaboratoriesMedsSearchDto()
+
         offset = (page - 1) * self.ITEMS_PER_PAGE
         limit = self.ITEMS_PER_PAGE * page
 
-        query = LaboratoriesMeds.objects.select_related('laboratory', 'med__substance')
+        query = self.get_query(search_dto)
+
         result = query.all().values(
-            lab_name=F('laboratory__name'),
-            lab_cnpj=F('laboratory__cnpj'),
-            substance_name=F('med__substance__name')
+            laboratory_name=F('laboratory__name'),
+            laboratory_cnpj=F('laboratory__cnpj'),
+            med_substance=F('med__substance__name')
         ).distinct()[offset:limit]
 
         return {
-            'total_pages': self.get_total_pages(),
+            'total_pages': self.get_total_pages(query),
             'data': result
         }
--- api/core/views.py
+++ api/core/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from .services import PmvgDataService
 from .repositories import LaboratoriesMedsRepository
+from .dtos import LaboratoriesMedsSearchDto
 
 
 class PmvgDataInsertView(APIView):
@@ -32,7 +33,14 @@ class MedsView(APIView):
         page = int(request.GET.get('page', FIRST_PAGE))
         page = page if page >= FIRST_PAGE else FIRST_PAGE
 
-        data = laboratories_meds_repository.get_by_page(page)
+        laboratories_meds_search_dto = LaboratoriesMedsSearchDto(
+            med_substance=request.GET.get('med_substance', ''),
+            laboratory_cnpj=request.GET.get('laboratory_cnpj', ''),
+            laboratory_name=request.GET.get('laboratory_name', ''),
+            term=request.GET.get('term', ''),
+        )
+
+        data = laboratories_meds_repository.get_by_page(page, laboratories_meds_search_dto)
         response_http_status = status.HTTP_200_OK if len(data['data']) > 0 else status.HTTP_204_NO_CONTENT
 
         return Response(
