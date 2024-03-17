c16f68a4dff43e6ae30120a5ac630e2997f3bd92
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-03-17 00:30:09 -0300

    add: disable default logging config - TIME_ZONE changed to America/Sao_Paulo

--- api/api/settings.py
+++ api/api/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING_CONFIG = None
