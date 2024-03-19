d7aaac885970f41ecdc19b900901d3bb30704794
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2024-03-19 08:41:39 -0300

    add: docker

---
+++ scripts/commands.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+python manage.py migrate
+python manage.py runserver
